c79c59f7c024e5d2003df0787f288c2e165b9b63
Gilles Regamey <gillesoff@gmail.com>
2024-03-10 12:10:03 +0100

    corrected sqrt in d meas

--- Haptics/matlab/gilles_penthograph.m
+++ Haptics/matlab/gilles_penthograph.m
@@ -7,12 +7,12 @@ c = 60;
 
 %% 7.2 1B
 phia = pi/2
-phib = 0
+phib = pi/2
 P1 = a*[sin(phia);cos(phia)] + [0;c/2]; 
 P2 = a*[sin(phib);cos(phib)] - [0;c/2]; 
 PM = (P1+P2)./2;
 nPM = norm(PM,2);
-PmP = b^2 -((a*sin(phia) - a*sin(phib))/2)^2 -((a*cos(phia) - a*cos(phib) - c)/2)^2;
+PmP = sqrt(b^2 -((a*sin(phia) - a*sin(phib))/2)^2 -((a*cos(phia) - a*cos(phib) - c)/2)^2);
 P = PM.*(PmP/nPM) + PM
 
 %% 7.2 1C
@@ -21,8 +21,8 @@ P1 = LENGTH_A*[sin(phi_A);cos(phi_A)] + [0;LENGTH_C/2];
 P2 = LENGTH_A*[sin(phi_B);cos(phi_B)] - [0;LENGTH_C/2]; 
 PM = (P1+P2)./2;
 nPM = norm(PM,2);
-PmP = LENGTH_B^2 -((LENGTH_A*sin(phi_A) - LENGTH_A*sin(phi_B))/2)^2 ...
-      -((LENGTH_A*cos(phi_A) - LENGTH_A*cos(phi_B) - LENGTH_C)/2)^2;
+PmP = sqrt(LENGTH_B^2 -((LENGTH_A*sin(phi_A) - LENGTH_A*sin(phi_B))/2)^2 ...
+      -((LENGTH_A*cos(phi_A) - LENGTH_A*cos(phi_B) - LENGTH_C)/2)^2);
 P = PM.*(PmP/nPM) + PM;
 fwcode = ccode(P,'File','./penthofwkin.c');
 
